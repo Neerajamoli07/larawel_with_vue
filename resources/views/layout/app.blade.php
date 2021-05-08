ec87fc7b86a63fb3e94a2f6a74d3d90fcc11cdeb
Neeraj Amoli <neeraj.amoli200@gmail.com>
2021-05-08 11:57:08 +0530

    crud functionality added with vue js

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" value="{{ csrf_token() }}" />
+    <title>Vue crud in Laravel</title>
+
+    <link href="{{ mix('css/app.css')}}" type="text/css" rel="stylesheet"/>
+
+</head>
+
+<body>
+    <div id="app"></div>
+    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
